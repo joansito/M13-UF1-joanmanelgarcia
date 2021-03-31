d1acb184154a04b1aecc8ef07cfe9a23b6330a82
joansito <74136459+joansito@users.noreply.github.com>
2021-03-31 17:06:49 +0200

    ArchivosProyecto

---
+++ Mini1-Prototipo/Assets/Mini3-rings/aro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aro : MonoBehaviour
+{
+    public GameObject refGiro;
+    public float vGiro;
+    Vector3 axis = new Vector3(0, 0, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 point = refGiro.transform.position;
+
+        transform.RotateAround(point, axis, Time.deltaTime * vGiro);
+
+        this.transform.localScale += new Vector3(0.005f,0.005f,0);
+    }
+}
