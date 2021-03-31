863d5b050115937214fd6f3da4d00729b28d587f
joansito <74136459+joansito@users.noreply.github.com>
2021-03-31 17:35:56 +0200

    Juego Base
    Juego Base con un parde prototipos en sucio

---
+++ Mini1-Prototipo/Assets/Mini1-run/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   public float giroVelocity = 1;
+   public float delanteSpeed = 0.06f;
+    public Rigidbody2D a;
+    void FixedUpdate()
+    {
+        
+        moveKeys();
+    }
+    private void moveKeys() {
+
+        if (Input.GetKey("right"))
+        {
+            transform.eulerAngles -= Vector3.forward * giroVelocity;
+            a.AddForce(transform.up.normalized * delanteSpeed*7);
+            a.velocity = a.velocity.normalized * delanteSpeed;
+        }
+        else if (Input.GetKey("left"))
+        {
+            a.AddForce(transform.up.normalized * delanteSpeed*7);
+            a.velocity = a.velocity.normalized * delanteSpeed;
+            transform.eulerAngles += Vector3.forward * giroVelocity;
+        }
+        else {
+            a.AddForce(transform.up.normalized * delanteSpeed);
+            a.velocity = a.velocity.normalized * delanteSpeed;
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8) {
+            Destroy(collision.gameObject);
+            print("punto");
+        }
+        else {
+            this.transform.position = new Vector2(-4.9f, 0);
+            print("teltransporte!!");
+        }
+        
+    }
+    
+  
+}
---
+++ Mini1-Prototipo/Assets/Mini3-rings/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject aroBlack;
+    public GameObject aroWhite;
+    public GameObject refGiro;
+    public float vGiro;
+    Vector3 axis = new Vector3(0, 0, 1);
+    // Start is called before the first frame update
+    void Update()
+    {
+        instantiateAro();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 point = refGiro.transform.position;
+
+        transform.RotateAround(point, axis, Time.deltaTime * vGiro);
+      
+    }
+
+    private void instantiateAro() {
+        if (Input.GetKeyUp("up"))
+        {
+            Instantiate(aroWhite, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+        } else if (Input.GetKeyUp("down")) {
+            Instantiate(aroBlack, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+        }
+    }
+}
