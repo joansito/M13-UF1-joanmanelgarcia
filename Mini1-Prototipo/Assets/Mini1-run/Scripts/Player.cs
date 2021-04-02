123805513de580853b81f27dc6dc8a415fb0c491
joansito <74136459+joansito@users.noreply.github.com>
2021-04-02 10:51:59 +0200

    Cambios
    Adición de marcadores, contadores de tiempo, contador de vidas etc

--- Mini1-Prototipo/Assets/Mini1-run/Scripts/Player.cs
+++ Mini1-Prototipo/Assets/Mini1-run/Scripts/Player.cs
@@ -6,13 +6,30 @@ public class Player : MonoBehaviour
 {
    public float giroVelocity = 1;
    public float delanteSpeed = 0.06f;
-    public Rigidbody2D a;
+   public Rigidbody2D a;
+   bool muerto;
+
+   HealthbarScript playerLives;
+    ScorePoints playerPoints;
+    CollectionableBar playerObjects;
+    public void Start()
+    {
+        playerLives = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<HealthbarScript>();
+        playerPoints= GameObject.FindGameObjectWithTag("ScoreBar").GetComponent<ScorePoints>();
+        playerObjects = GameObject.FindGameObjectWithTag("CollectionableBar").GetComponent<CollectionableBar>();
+    }
     void FixedUpdate()
     {
-        
+        muerto = playerLives.getMuerte();
+        if (muerto) { print("muerto"); }
         moveKeys();
     }
     private void moveKeys() {
+
+        if (Input.GetKey("a")) {
+            playerLives.setVida(-1);
+            print("A!!");
+        }
 
         if (Input.GetKey("right"))
         {
@@ -38,8 +55,17 @@ public class Player : MonoBehaviour
         if (collision.gameObject.layer == 8) {
             Destroy(collision.gameObject);
             print("punto");
-        }
-        else {
+            playerPoints.setPuntos(100);
+            playerObjects.setColeccionable(1);
+
+        } else if (collision.gameObject.layer==9) {
+            Destroy(collision.gameObject);
+            print("disparorecibido");
+            if (!muerto)
+            {
+                playerLives.setVida(-1);
+            }
+        }else {
             this.transform.position = new Vector2(-4.9f, 0);
             print("teltransporte!!");
         }
